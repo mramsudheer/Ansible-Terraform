17ce7bd1aa8abe9a28dbb4b230cc9aee24fe967b
Sudheer <rama.sudheer@accenture.com>
2026-03-19 13:14:28 +0530

    Redis Route53

--- infrastructure-live/dev/us-east-1/databases/route53.tf
+++ infrastructure-live/dev/us-east-1/databases/route53.tf
@@ -6,3 +6,11 @@ resource "aws_route53_record" "mongodb_r53" {
   records         = [aws_instance.mongodb_instance.private_ip]
   allow_overwrite = true
 }
+resource "aws_route53_record" "redis_r53" {
+  zone_id         = var.hosted_zone_id
+  name            = "redis-${var.env}.${var.domain_name}"
+  type            = "A"
+  ttl             = "1"
+  records         = [aws_instance.redis_instance.private_ip]
+  allow_overwrite = true
+}
