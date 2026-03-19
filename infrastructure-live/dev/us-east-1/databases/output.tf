ca845eb46765385480d0192b77fd65b736eb87a6
Sudheer <rama.sudheer@accenture.com>
2026-03-19 17:17:37 +0530

    all

--- infrastructure-live/dev/us-east-1/databases/output.tf
+++ infrastructure-live/dev/us-east-1/databases/output.tf
@@ -18,5 +18,23 @@ output "mongodb_security_group" {
 output "mongodb_r53_record" {
   value       = aws_route53_record.mongodb_r53.name
   description = "mongodb Route53 Record"
+}
+# FOR REDIS
+output "redis_instance_id" {
+  value       = aws_instance.redis_instance.id
+  description = "redis Instance Id"
+}
 
+output "redis_private_ip" {
+  value       = aws_instance.redis_instance.private_ip
+  description = "redis Private IP"
+}
+output "redis_security_group" {
+  #value = aws_instance.redis.vpc_security_group_ids
+  value       = nonsensitive(tolist(aws_instance.redis_instance.vpc_security_group_ids)[0])
+  description = "redis Security Group"
+}
+output "redis_r53_record" {
+  value       = aws_route53_record.redis_r53.name
+  description = "redis Route53 Record"
 }
