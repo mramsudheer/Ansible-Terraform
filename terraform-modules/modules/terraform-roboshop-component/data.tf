0edf1ab744ca0c4e39c2dfa17087590b54862cc7
Sudheer <rama.sudheer@accenture.com>
2026-03-26 11:11:38 +0530

    Components Error Fix

--- terraform-modules/modules/terraform-roboshop-component/data.tf
+++ terraform-modules/modules/terraform-roboshop-component/data.tf
@@ -12,7 +12,7 @@ data "aws_ssm_parameter" "vpc_id" {
 }
 # 2. Fetch Private Subnet IDs from SSM
 data "aws_ssm_parameter" "private_subnet_ids" {
-  name = "/${title(var.project_name)}/${title(var.environment)}/vpc/private-subnet-ids"
+  name = "/${title(var.project_name)}/${title(var.environment)}/vpc/private_subnet_ids"
 }
 data "aws_ssm_parameter" "sg_id" {
   name = "/${title(var.project_name)}/${title(var.environment)}/${title(var.component)}_sg-id"
@@ -21,7 +21,7 @@ data "aws_ssm_parameter" "sg_id" {
 #   name = "/${title(var.project_name)}/${title(var.environment)}/Backend_alb_sg-id"
 # }
 data "aws_ssm_parameter" "backend_alb_listener_arn" {
-  name = "/${title(var.project_name)}/${title(var.environment)}/alb/backend_listener_arn_listener_arn"
+  name = "/${title(var.project_name)}/${title(var.environment)}/alb/backend_listener_arn"
 }
 data "aws_ssm_parameter" "frontend_listener_arn" {
   name = "/${title(var.project_name)}/${title(var.environment)}/alb/frontend_listener_arn"
